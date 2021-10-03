847baa391c35e4e1790bf2669728c0e0980f30f5
Aboulaye Drabo <drabis47@gmail.com>
2021-10-03 11:22:47 -0400

    yaml file added

--- M2-Challenge-Aboulaye-Drabo/src/main/java/com/company/controller/CalculatorController.java
+++ M2-Challenge-Aboulaye-Drabo/src/main/java/com/company/controller/CalculatorController.java
@@ -1,15 +1,13 @@
 package com.company.controller;
 
 import com.company.models.Calculator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 import java.util.Objects;
+
 
 @RestController
 public class CalculatorController {
